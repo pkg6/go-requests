6052f50914f541e25c02b4fce3bedf62a14fbee7
chihqiang <zhiqiang2033@gmail.com>
2023-06-17 23:43:01 +0800

    fix: requests

--- requests.go
+++ requests.go
@@ -8,97 +8,97 @@ import (
 //requests helper function
 
 func Get(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Get(context.Background(), uri, data)
+	return client.Get(context.Background(), uri, data)
 }
 
 func Put(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Put(context.Background(), uri, data)
+	return client.Put(context.Background(), uri, data)
 }
 
 func Delete(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Delete(context.Background(), uri, data)
+	return client.Delete(context.Background(), uri, data)
 }
 
 func Head(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Head(context.Background(), uri, data)
+	return client.Head(context.Background(), uri, data)
 }
 
 func Patch(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Patch(context.Background(), uri, data)
+	return client.Patch(context.Background(), uri, data)
 }
 
 func Connect(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Connect(context.Background(), uri, data)
+	return client.Connect(context.Background(), uri, data)
 }
 
 func Options(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Options(context.Background(), uri, data)
+	return client.Options(context.Background(), uri, data)
 }
 
 func Trace(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Trace(context.Background(), uri, data)
+	return client.Trace(context.Background(), uri, data)
 }
 
 func Post(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.Post(context.Background(), uri, data)
+	return client.Post(context.Background(), uri, data)
 }
 
 func PostJson(uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.PostJson(context.Background(), uri, data)
+	return client.PostJson(context.Background(), uri, data)
 }
 
 func PostForm(uri string, data url.Values, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.PostForm(context.Background(), uri, data)
+	return client.PostForm(context.Background(), uri, data)
 }
 
 func Request(method, uri string, data any, args ...func(client *Client)) (*Response, error) {
-	requests := new(Client).Clone()
+	client := new(Client).Clone().WitchHttpClient(defaultHttpClient(nil))
 	for _, arg := range args {
-		arg(requests)
+		arg(client)
 	}
-	return requests.DoRequest(context.Background(), method, uri, data)
+	return client.DoRequest(context.Background(), method, uri, data)
 }
