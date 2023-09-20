03fc2d5b9f509b24a62cb2ba1c56a8f2d0cde0f0
zhiqiangwang <zhiqiangwang@aliyun.com>
2023-09-20 22:05:58 +0800

    fix: requestLogger

--- callbacks.go
+++ callbacks.go
@@ -1,21 +1,33 @@
 package requests
 
 import (
+	"context"
 	"fmt"
+	"github.com/pkg6/go-requests/jsons"
 	"io"
 	"net/http"
 	"time"
 )
 
+const requestLogCtxKey = "request_log_star_time"
+
 func requestLogger(client *Client, request *http.Request) error {
 	if client.Debug {
+		now := time.Now()
+		var body []byte
+		if request.Body != nil {
+			body, _ = io.ReadAll(request.Body)
+			request.Body = NewReadCloser(body, false)
+		}
+		client.Context = context.WithValue(context.Background(), requestLogCtxKey, now)
+		headers, _ := jsons.Marshal(request.Header)
 		reqLog := "\n==============================================================================\n" +
 			"~~~ REQUEST ~~~\n" +
 			fmt.Sprintf("%s  %s  %s\n", request.Method, request.URL.RequestURI(), request.Proto) +
-			fmt.Sprintf("HOST   : %s\n", request.URL.Host) +
-			fmt.Sprintf("HEADERS:\n%s\n", request.Header) +
-			fmt.Sprintf("TIME DURATION: %v\n", time.Now()) +
-			fmt.Sprintf("BODY   :\n%v\n", request.Body) +
+			fmt.Sprintf("HOST           : %s\n", request.URL.Host) +
+			fmt.Sprintf("TIME DURATION  : %v\n", now.Format(time.RFC3339Nano)) +
+			fmt.Sprintf("HEADERS        : \n%s\n", string(headers)) +
+			fmt.Sprintf("BODY           :\n%v\n", string(body)) +
 			"------------------------------------------------------------------------------\n"
 		client.Logger.Println(reqLog)
 	}
@@ -23,20 +35,24 @@ func requestLogger(client *Client, request *http.Request) error {
 }
 func responseLogger(client *Client, request *http.Request, response *Response) error {
 	if client.Debug {
+		e := time.Now()
 		var reqBodyContent []byte
 		if response.Body != nil {
 			reqBodyContent, _ = io.ReadAll(response.Body)
 			response.requestBody = reqBodyContent
 			response.Body = NewReadCloser(reqBodyContent, false)
 		}
+		s := client.Context.Value(requestLogCtxKey).(time.Time)
+		headers, _ := jsons.Marshal(response.Header)
 		debugLog := "~~~ RESPONSE ~~~\n" +
+			fmt.Sprintf("CLONE        : %v\n", client.clone) +
 			fmt.Sprintf("STATUS       : %s\n", response.Status) +
 			fmt.Sprintf("PROTO        : %s\n", response.Proto) +
 			fmt.Sprintf("ATTEMPT      : %v\n", client.attempt) +
-			fmt.Sprintf("RECEIVED AT  : %v\n", time.Now().Format(time.RFC3339Nano)) +
-			fmt.Sprintf("TIME DURATION: %v\n", time.Now()) +
-			fmt.Sprintf("HEADERS: %v\n", response.Header)
-		debugLog += fmt.Sprintf("BODY: %s\n", string(reqBodyContent))
+			fmt.Sprintf("RECEIVED AT  : %v\n", e.Format(time.RFC3339Nano)) +
+			fmt.Sprintf("HEADERS      : \n%v\n", string(headers))
+		debugLog += fmt.Sprintf("BODY : \n%s\n", string(reqBodyContent))
+		debugLog += fmt.Sprintf("TIME CONSUMING : %v\n", e.Sub(s))
 		debugLog += "==============================================================================\n"
 		client.Logger.Println(debugLog)
 	}
