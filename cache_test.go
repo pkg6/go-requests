96fe99895dcaba1f7a389b56865bdb082216d3ee
wángzhìqiáng <40115555+zzqqw@users.noreply.github.com>
2023-10-01 01:48:53 +0800

    github cli go test

--- cache_test.go
+++ cache_test.go
@@ -8,36 +8,37 @@ import (
 	"time"
 )
 
+var (
+	_newCache = NewFileCache("./.testdata/cache")
+)
+
 // 测试过期的缓存被驱逐
 func TestCacheEvict(t *testing.T) {
-	cache := NewFileCache()
-	err := cache.Set("test", "value", 1*time.Second)
+	err := _newCache.Set("test", "value", 1*time.Second)
 	require.NoError(t, err)
 	time.Sleep(2 * time.Second) // 等待过期
-	_, err = cache.Get("test")
+	_, err = _newCache.Get("test")
 	require.Error(t, err) // 应该获取不到
 }
 
 // 测试设置和获取字符串缓存
 func TestSetGet(t *testing.T) {
-	cache := NewFileCache()
 	key := "testkey"
 	value := "test value"
-	err := cache.Set(key, value, 0)
+	err := _newCache.Set(key, value, 0)
 	require.NoError(t, err)
-	v, err := cache.Get(key)
+	v, err := _newCache.Get(key)
 	require.NoError(t, err)
 	require.Equal(t, v, value)
 }
 
 func BenchmarkGet(b *testing.B) {
-	cache := NewFileCache()
 	for i := 0; i < b.N; i++ {
 		key := fmt.Sprintf("key%d", i)
-		cache.Set(key, strings.Repeat("a", 100), 0)
+		_newCache.Set(key, strings.Repeat("a", 100), 0)
 	}
 	for i := 0; i < b.N; i++ {
 		key := fmt.Sprintf("key%d", i)
-		cache.Get(key)
+		_newCache.Get(key)
 	}
 }
