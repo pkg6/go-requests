a9727ab14d7ef2ae4bb7cdeb9ba0ce77afdf9406
chihqiang <zhiqiang2033@gmail.com>
2023-10-04 21:11:55 +0800

    feat: WithCookieNextRequest

--- cookie.go
+++ cookie.go
@@ -1,8 +1,56 @@
 package requests
 
-type Cookie map[string][]string
+import (
+	"net/http"
+	"strings"
+	"time"
+)
 
-func (v Cookie) Get(key string) string {
+func OnAfterRequestWithCookie(cache ICache) RequestCallback {
+	return func(client *Client, request *http.Request) error {
+		cacheKey := Md5(request.URL.Host)
+		if cache.Has(cacheKey) {
+			if cookStr, _ := cache.Get(cacheKey); cookStr != "" {
+				var cookieRaw CookieRaw
+				_ = client.JSONUnmarshal([]byte(cookStr), &cookieRaw)
+				request.Header.Set(HttpHeaderCookie, cookieRaw.Encode())
+			}
+		}
+		return nil
+	}
+}
+
+func OnResponseWithCookie(cache ICache, ttl time.Duration) ResponseCallback {
+	return func(client *Client, request *http.Request, response *Response) error {
+		cacheKey := Md5(request.URL.Host)
+		if !cache.Has(cacheKey) {
+			cookieRaw := response.GetCookieMap()
+			if len(cookieRaw) > 0 {
+				cookieByte, _ := client.JSONMarshal(cookieRaw)
+				_ = cache.Set(cacheKey, string(cookieByte), ttl)
+			}
+		}
+		return nil
+	}
+}
+
+func CookieToRaw(cookie string) CookieRaw {
+	cookieRaw := make(CookieRaw)
+	if cookie == "" {
+		return cookieRaw
+	}
+	for _, pair := range strings.Split(cookie, ";") {
+		kv := strings.SplitN(pair, "=", 2)
+		if len(kv) == 2 {
+			cookieRaw.Set(kv[0], kv[1])
+		}
+	}
+	return cookieRaw
+}
+
+type CookieRaw map[string][]string
+
+func (v CookieRaw) Get(key string) string {
 	if v == nil {
 		return ""
 	}
@@ -12,20 +60,20 @@ func (v Cookie) Get(key string) string {
 	}
 	return vs[0]
 }
-func (v Cookie) Set(key, value string) {
+func (v CookieRaw) Set(key, value string) {
 	v[key] = []string{value}
 }
-func (v Cookie) Add(key, value string) {
+func (v CookieRaw) Add(key, value string) {
 	v[key] = append(v[key], value)
 }
-func (v Cookie) Del(key string) {
+func (v CookieRaw) Del(key string) {
 	delete(v, key)
 }
-func (v Cookie) Has(key string) bool {
+func (v CookieRaw) Has(key string) bool {
 	_, ok := v[key]
 	return ok
 }
-func (v Cookie) Encode() string {
+func (v CookieRaw) Encode() string {
 	cookieStr := ""
 	for s := range v {
 		if cookieStr != "" {
